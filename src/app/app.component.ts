5deca9b17b1302aa34148ffb05a7358ff3d27980
Santiago Rodriguez <santiagorodriguez@MacBook-Pro-de-Santiago.local>
2020-01-27 14:06:21 -0500

    selector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent implements OnInit {
   title = 'dexF';
   filtro: any;
   paisesFiltered: any;
+  opcionFilter: any;
   constructor(private http: HttpClient) {
 
   }
@@ -20,14 +21,39 @@ export class AppComponent implements OnInit {
       .subscribe((resp: any) => {
         this.paises = resp;
         this.paisesFiltered = resp;
+        console.log(resp);
       });
+  }
+
+  changeFilter(event){
+    let x = event.target.value;
+    this.opcionFilter = x;
   }
 
   searchFilter(event) {
     if (!event.target.value) { return this.paisesFiltered = this.paises; }
-    this.paisesFiltered = [...new Set([
-      ...this.paises.filter(({ name }) => name.toLowerCase().startsWith(event.target.value)),
+    let filtro = event.target.value;
+    switch (this.opcionFilter) {
+      case 'Languagues':
+        this.paisesFiltered = [...new Set([
+      ...this.paises.filter(({languages}) => languages.find((x) => x.name.toLowerCase().startsWith(filtro))),
     ])];
+        break;
+      case 'Country':
+        this.paisesFiltered = [...new Set([
+          ...this.paises.filter(({ name }) => name.toLowerCase().startsWith(filtro)),
+        ])];
+      break;
+      case 'Continent':
+        this.paisesFiltered = [...new Set([
+          ...this.paises.filter(({ region }) => region.toLowerCase().startsWith(filtro)),
+        ])];
+      break;
+    }
+    // this.paisesFiltered = [...new Set([
+    //   ...this.paises.filter(({ name }) => name.toLowerCase().startsWith(filtro)),
+    //   ...this.paises.filter(({languages}) => languages.find((x) => x.nativeName.toLowerCase().startsWith(filtro))),
+    // ])];
     console.log(event.target.value);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CardCountryComponent } from './components/card-country/card-country.component';
-import { HomeComponent } from './components/home/home.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { MapaComponent } from './components/mapa/mapa.component';
+import { CardCountryComponent } from './components/card-country/card-country.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     CardCountryComponent,
-    HomeComponent,
-    MapaComponent
+    MapaComponent,
   ],
   imports: [
     BrowserModule,
